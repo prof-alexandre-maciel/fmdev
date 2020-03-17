25cd77e26784d980012606b27fa3b8ed2c456e9c
Raniel <raniel@Raniels-MacBook-Pro.local>
2020-03-17 15:04:06 -0300

    Init endpoint boxplot

---
+++ backend/resources/Boxplot.py
@@ -0,0 +1,38 @@
+import traceback
+import pandas as pd
+from utils import utils
+from flask import request
+from flask_restful import Resource
+
+
+class Boxplot(Resource):
+
+    def get_csv_data(self, path, indicator):
+        res = {'status': 'success'}
+
+        try:
+            df = pd.read_csv(path)
+            res['data'] = df[indicator]
+        except:
+            res['status'] = 'error'
+
+        return res
+
+    def post(self):
+        try:
+            path = None
+            payload = request.get_json()
+
+            if 'path' not in payload or payload['path'] is None:
+                return {"msg": "Invalid path"}, 500
+
+            res = self.get_csv_data(payload['path'], payload['indicator'])
+
+            if res['status'] == 'error':
+                return {"msg": "Invalid path"}, 500
+            
+            return res['data']
+
+        except:
+            traceback.print_exc()
+            return {"msg": "Error on POST Boxplot"}, 500
