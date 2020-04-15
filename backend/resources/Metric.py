29e50d3f6293bf94101fde159261b6d386ed46be
Raniel <raniel@Raniels-MacBook-Pro.local>
2020-04-15 20:28:32 -0300

    return initial metrics

--- backend/resources/Metric.py
+++ backend/resources/Metric.py
@@ -1,16 +1,30 @@
 import joblib
 import traceback
+import pandas as pd
 from utils import utils
-from flask import request, current_app
 from flask_restful import Resource
+from sklearn.metrics import SCORERS
+from flask import request, current_app
 
 
 class Metric(Resource):
+    
+    def get_test_data(self, split_type):
+        filename = utils.get_filename_from_path(request, '.csv')
+        filename = f"{current_app.config.get(split_type)}/{filename}"
+        data = pd.read_csv(filename)
 
-    def get_metrics(tpot):
-        precision = SCORERS['precision'](tpot, X_test, Y_test)
-        recall = SCORERS['recall'](tpot, X_test, Y_test)
-        roc_auc = SCORERS['roc_auc'](tpot, X_test, Y_test)
+        return data
+
+    def get_metrics(self, tpot, x_test, y_test):
+        metrics = {
+            'score': tpot.score(x_test, y_test),
+            'precision': SCORERS['precision'](tpot, x_test, y_test),
+            'recall': SCORERS['recall'](tpot, x_test, y_test),
+            'roc_auc': SCORERS['roc_auc'](tpot, x_test, y_test)
+        }
+
+        return metrics
 
     def load_model(self):
         filename = utils.get_filename_from_path(request, '')
@@ -22,10 +36,11 @@ class Metric(Resource):
     def post(self):
         try:
             tpot = self.load_model()
+            x_test = self.get_test_data('TEST_FEATURES')
+            y_test = self.get_test_data('TEST_TARGET')
+            metrics = self.get_metrics(tpot, x_test, y_test)
 
-            
-            
-            return {}
+            return metrics
 
         except:
             traceback.print_exc()
