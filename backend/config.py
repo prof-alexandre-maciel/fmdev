5f5320e11941b79524279584b8750f75319452b0
Raniel <raniel@Raniels-MacBook-Pro.local>
2020-05-21 01:27:09 -0300

    python read environs dynamically by FLASK_ENV

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,19 @@
 import os
+from environs import Env
+
+env = Env()
+
+if os.environ.get('FLASK_ENV') == 'production':
+    env.read_env()
+else:
+    env.read_env('.env.development')
 
 # You need to replace the next values with the appropriate values for your configuration
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "postgresql://raniel:@localhost/fmdev"
+SQLALCHEMY_DATABASE_URI = f"postgresql://{env.str('DB_USER')}:{env.str('DB_PWD')}@{env.str('DB_HOST')}:{env.str('DB_PORT')}/{env.str('DB_NAME')}"
 JWT_SECRET_KEY = 'secret'
 PRE_PROCESSING_RAW = 'data/pre_processing'
 TRAIN_MODELS = 'data/models'
